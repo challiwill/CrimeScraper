bc3d8da83372329d65fc0677c02ed9a27b509f5f
Corbin Halliwill <corbin.halliwill@berkeley.edu>
2014-11-09 21:02:16 -0800

    loop through all crime types

--- scrape_crime.js
+++ scrape_crime.js
@@ -13,8 +13,26 @@ xmax = '-13603260.046893662';
 ymax = '4563184.9737941185';
 crimes = ['AS', 'AR'];
 
-for (crime in crimes) {
-    var url = 'http://www.crimemapping.com/DetailedReport.aspx?db='+bmonth+'/'+bday+'/'+byear+'+00:00:00&de='+emonth+'/'+eday+'/'+eyear+'+23:59:00&ccs='+crimes[crime]+'&xmin='+xmin+'&ymin='+ymin+'&xmax='+xmax+'&ymax='+ymax;
+crime_types = {
+    'arson' : 'AR',
+    'assault' : 'AS',
+    'burglary' : 'BU',
+    'disturbing the peace' : 'DP',
+    'drugs/alcohol violations' : 'DR',
+    'dui' : 'DU',
+    'fraud' : 'FR',
+    'homicide' : 'HO',
+    'motor vehicle theft' : 'VT',
+    'robbery' : 'RO',
+    'sec crimes' : 'SX',
+    'theft/larceny' : 'TH',
+    'vandalism' : 'VA',
+    'vehicle break-in/theft' : 'VB',
+    'weapons' : 'WE'
+};
+
+for (crime in crime_types) {
+    var url = 'http://www.crimemapping.com/DetailedReport.aspx?db='+bmonth+'/'+bday+'/'+byear+'+00:00:00&de='+emonth+'/'+eday+'/'+eyear+'+23:59:00&ccs='+crime_types[crime]+'&xmin='+xmin+'&ymin='+ymin+'&xmax='+xmax+'&ymax='+ymax;
     request(url, (function(crime) { return function(err, resp, body) {
     	$ = cheerio.load(body);
     	$('.report tr').each(function() {
